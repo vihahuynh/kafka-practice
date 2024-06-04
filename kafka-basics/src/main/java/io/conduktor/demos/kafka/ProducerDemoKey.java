b27105a0c75be4bd2b536ca6144d66c9f2e5702c
vihahuynh <huynhviha1703@gmail.com>
2024-06-04 20:02:07 +0700

    section 9: java producer with key

---
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoKey.java
@@ -0,0 +1,58 @@
+package io.conduktor.demos.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoKey {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoKey.class.getSimpleName());
+
+    public static void main(String[] args) throws InterruptedException {
+       log.info("Hello World");
+
+        // create Producer Properties
+        Properties properties = new Properties();
+
+        // localhost
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // set Producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a producer record
+        for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < 10; i++) {
+                String topic = "demo_java";
+                String key = "id_" + i;
+                String value = "hello world" + i;
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topic, key, value);
+
+                // send data
+                producer.send(producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                        // executes every times a record successfully sent a record or an exception is thrown
+                        if (e == null) {
+                            log.info("Key: " + key + " | Partition: " + recordMetadata.partition());
+                        }else {
+                            log.error("Error while sending message", e);
+                        }
+                    }
+                });
+            }
+        }
+        // flush and close the producer
+        // flush: tell producer to send all data and block until done --> synchronize
+        producer.flush();
+
+        // close: also call producer.flush before doing this
+        producer.close();
+    }
+}
